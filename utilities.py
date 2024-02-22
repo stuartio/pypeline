e688103233edfd00254f09eaef4aae88d765639b
Stuart Macleod <smacleod@akamai.com>
2024-02-21 16:16:18 -0800

    Stripping leading and trailing spaces from sanitised rule names

--- utilities.py
+++ utilities.py
@@ -5,24 +5,19 @@ from jsonpath_ng.ext import parse
 from akamai.edgegrid import EdgeRc
 import click
 
+
 def sanitizeFileName(unsafe_filename):
     safe_filename = unsafe_filename
-    bad_characters = [
-        '\\',
-        '/',
-        ':',
-        '*',
-        '?',
-        '"',
-        '<',
-        '>',
-        '|'
-    ]
+    bad_characters = ["\\", "/", ":", "*", "?", '"', "<", ">", "|"]
 
     for bad_char in bad_characters:
-        encoded_char = urllib.parse.quote(bad_char, safe='')
+        encoded_char = urllib.parse.quote(bad_char, safe="")
         safe_filename = safe_filename.replace(bad_char, encoded_char)
+
+    # Trim trailing spaces
+    safe_filename = safe_filename.strip()
     return safe_filename
+
 
 def split_child_rule(rule, directory):
     # print('Parsing rule: ' + rule['name'])
@@ -30,18 +25,18 @@ def split_child_rule(rule, directory):
         # print('Making directory: ' + directory)
         os.mkdir(directory)
 
-    sanitized_rule_name = sanitizeFileName(rule['name'])
-    children_directory = directory + '/' + sanitized_rule_name
-    
-    for index, child in enumerate(rule['children']):
-        child_filename = sanitizeFileName(child['name'])
-        split_child_rule(child, children_directory)
-        rule['children'][index] = '#include:{d}/{f}.json'.format(d = sanitized_rule_name, f = child_filename)
+    sanitized_rule_name = sanitizeFileName(rule["name"])
+    children_directory = directory + "/" + sanitized_rule_name
 
-    rule_filename = directory + '/' + sanitized_rule_name + '.json'
-    with open(rule_filename, 'w') as rule_file:
-        json.dump(rule, rule_file, indent = 2)
-    
+    for index, child in enumerate(rule["children"]):
+        child_filename = sanitizeFileName(child["name"])
+        split_child_rule(child, children_directory)
+        rule["children"][index] = "#include:{d}/{f}.json".format(d=sanitized_rule_name, f=child_filename)
+
+    rule_filename = directory + "/" + sanitized_rule_name + ".json"
+    with open(rule_filename, "w") as rule_file:
+        json.dump(rule, rule_file, indent=2)
+
 
 def split_rules(rules, output_directory):
     ## Check Output Directory and create if required
@@ -50,90 +45,94 @@ def split_rules(rules, output_directory):
         os.mkdir(output_directory)
 
     ## Iterate through child rules
-    for index, child in enumerate(rules['rules']['children']):
-        child_filename = sanitizeFileName(child['name']) + '.json'
+    for index, child in enumerate(rules["rules"]["children"]):
+        child_filename = sanitizeFileName(child["name"]) + ".json"
         split_child_rule(child, output_directory)
-        rules['rules']['children'][index] = '#include:' + child_filename
-    
-    ## Create variables file
-    variables_filename = output_directory + '/pmVariables.json'
-    with open(variables_filename, 'w') as variables_file:
-        json.dump(rules['rules']['variables'], variables_file, indent = 2)
-    rules['rules']['variables'] = '#include:pmVariables.json'
+        rules["rules"]["children"][index] = "#include:" + child_filename
 
-    main_filename = output_directory + '/main.json'
-    with open(main_filename, 'w') as main_file:
-        json.dump(rules['rules'], main_file, indent=2)
+    ## Create variables file
+    variables_filename = output_directory + "/pmVariables.json"
+    with open(variables_filename, "w") as variables_file:
+        json.dump(rules["rules"]["variables"], variables_file, indent=2)
+    rules["rules"]["variables"] = "#include:pmVariables.json"
+
+    main_filename = output_directory + "/main.json"
+    with open(main_filename, "w") as main_file:
+        json.dump(rules["rules"], main_file, indent=2)
+
 
 def merge_child_rule(file_path):
     # print('Loading file: ' + file_path)
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         rules = json.load(file)
     parent_file_dir = os.path.dirname(file_path)
-    child_file_prefix = parent_file_dir + '/'
+    child_file_prefix = parent_file_dir + "/"
 
-    for index, child in enumerate(rules['children']):
-        if isinstance(child, str) and '#include:' in child:
-            child_filename = child.replace('#include:',child_file_prefix)
+    for index, child in enumerate(rules["children"]):
+        if isinstance(child, str) and "#include:" in child:
+            child_filename = child.replace("#include:", child_file_prefix)
             child_rules = merge_child_rule(child_filename)
-            rules['children'][index] = child_rules
+            rules["children"][index] = child_rules
 
     return rules
+
 
 def merge_rules(main_file_path):
     rules = {}
     # print('Loading main file: ' + main_file_path)
-    with open(main_file_path, 'r') as file:
-        rules['rules'] = json.load(file)
+    with open(main_file_path, "r") as file:
+        rules["rules"] = json.load(file)
     parent_file_dir = os.path.dirname(main_file_path)
-    child_file_prefix = parent_file_dir + '/'
+    child_file_prefix = parent_file_dir + "/"
 
-    for index, child in enumerate(rules['rules']['children']):
-        if isinstance(child, str) and '#include:' in child:
-            child_filename = child.replace('#include:',child_file_prefix)
+    for index, child in enumerate(rules["rules"]["children"]):
+        if isinstance(child, str) and "#include:" in child:
+            child_filename = child.replace("#include:", child_file_prefix)
             child_rules = merge_child_rule(child_filename)
-            rules['rules']['children'][index] = child_rules
+            rules["rules"]["children"][index] = child_rules
 
-    if isinstance(rules['rules']['variables'], str) and '#include:' in rules['rules']['variables']:
-        variables_filename = rules['rules']['variables'].replace('#include:',child_file_prefix)
+    if isinstance(rules["rules"]["variables"], str) and "#include:" in rules["rules"]["variables"]:
+        variables_filename = rules["rules"]["variables"].replace("#include:", child_file_prefix)
         # print('Loading variables file: ' + variables_filename)
-        with open(variables_filename, 'r') as variables_file:
-            rules['rules']['variables'] = json.load(variables_file)
+        with open(variables_filename, "r") as variables_file:
+            rules["rules"]["variables"] = json.load(variables_file)
 
     return rules
 
+
 def get_credentials(edgerc_path, section, account_key):
-    credential_elements = ['host', 'client_token', 'access_token', 'client_secret', 'account_key']
+    credential_elements = ["host", "client_token", "access_token", "client_secret", "account_key"]
     credentials = {}
     edgerc = None
     if edgerc_path is None:
         env_elements_present = True
         ## if no edgerc path supplied, check for env variables first
         for element in credential_elements:
-            if section == 'default':
-                env_variable = 'AKAMAI_' + element.upper()
+            if section == "default":
+                env_variable = "AKAMAI_" + element.upper()
             else:
-                env_variable = 'AKAMAI_' + section.upper() + '_' + element.upper()
+                env_variable = "AKAMAI_" + section.upper() + "_" + element.upper()
             if os.getenv(env_variable) is not None:
                 credentials[element] = os.getenv(env_variable)
             else:
                 env_elements_present = False
-        
+
         ## If any edgerc elements missing, look for default EdgeRCFile location
         if not env_elements_present:
-            edgerc = EdgeRc('~/.edgerc')
+            edgerc = EdgeRc("~/.edgerc")
     elif os.path.exists(edgerc_path):
         edgerc = EdgeRc(edgerc_path)
     else:
-        raise(Exception('Specified EdgeRC File does not exist'))
-    
+        raise (Exception("Specified EdgeRC File does not exist"))
+
     if edgerc is not None:
         for element in credential_elements:
             credentials[element] = edgerc.get(section, element)
 
     if account_key is not None:
-        credentials['account_key'] = account_key
+        credentials["account_key"] = account_key
     return credentials
+
 
 def apply_variable_by_jsonpath(rules, path, value):
     jsonpath_expression = parse(path)
@@ -142,5 +141,5 @@ def apply_variable_by_jsonpath(rules, path, value):
     if len(rule_found) > 0:
         jsonpath_expression.update(rules, value)
     else:
-        click.secho('WARNING: Path "{p}" not found in supplied rules. No update performed'.format(p = path), fg = 'yellow')
+        click.secho('WARNING: Path "{p}" not found in supplied rules. No update performed'.format(p=path), fg="yellow")
     return rules
